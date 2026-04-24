0558eb1844b5560272dbc6fe41fc9d41d311049f
Carson Buttars <carsonbuttars13@gmail.com>
2026-04-24 16:34:23 -0600

    Split data cleaning process to separate file.

--- EDA.R
+++ EDA.R
@@ -4,63 +4,8 @@ library(ggplot2)
 # Read data #
 #############
 
-train <- read.csv("train.csv") |>
-  within({
-    gender <- as.factor(gender)
-    SeniorCitizen <- (SeniorCitizen == 1) |>
-      ifelse("Yes", "No") |>
-      as.factor()
-    Partner <- as.factor(Partner)
-    Dependents <- as.factor(Dependents)
-    PhoneService <- as.factor(PhoneService)
-    MultipleLines <- MultipleLines |>
-      (\(.) {
-         .[. == "No phone service"] <- "No"
-         .
-      })() |>
-      as.factor()
-    InternetService <- as.factor(InternetService)
-    OnlineSecurity <- OnlineSecurity |>
-      (\(.) {
-         .[. == "No internet service" ] <- "No"
-         .
-      })() |>
-      as.factor()
-    OnlineBackup <- OnlineBackup |>
-      (\(.) {
-         .[. == "No internet service" ] <- "No"
-         .
-      })() |>
-      as.factor()
-    DeviceProtection <- DeviceProtection |>
-      (\(.) {
-         .[. == "No internet service" ] <- "No"
-         .
-      })() |>
-      as.factor()
-    TechSupport <- TechSupport |>
-      (\(.) {
-         .[. == "No internet service" ] <- "No"
-         .
-      })() |>
-      as.factor()
-    StreamingTV <- StreamingTV |>
-      (\(.) {
-         .[. == "No internet service" ] <- "No"
-         .
-      })() |>
-      as.factor()
-    StreamingMovies <- StreamingMovies |>
-      (\(.) {
-         .[. == "No internet service" ] <- "No"
-         .
-      })() |>
-      as.factor()
-    Contract <- as.factor(Contract)
-    PaperlessBilling <- as.factor(PaperlessBilling)
-    PaymentMethod <- as.factor(PaymentMethod)
-    Churn <- as.factor(Churn)
-  })
+source("clean_data.R")
+
 
 #################
 # Check for NAs #
